e63b985662bdbe4c63274f06a3115741274db084
HaojunRen <1394997@qq.com>
2021-01-18 12:49:55 +0800

    thread.mdc.enabled默认设置为true

--- discovery-agent-starter-plugin-mdc/src/main/java/com/nepxion/discovery/agent/plugin/strategy/mdc/DiscoveryMDCPlugin.java
+++ discovery-agent-starter-plugin-mdc/src/main/java/com/nepxion/discovery/agent/plugin/strategy/mdc/DiscoveryMDCPlugin.java
@@ -12,7 +12,7 @@ package com.nepxion.discovery.agent.plugin.strategy.mdc;
 import com.nepxion.discovery.agent.plugin.AbstractPlugin;
 
 public class DiscoveryMDCPlugin extends AbstractPlugin {
-    private Boolean threadMDCEnabled = Boolean.valueOf(System.getProperty("thread.mdc.enabled", "false"));
+    private Boolean threadMDCEnabled = Boolean.valueOf(System.getProperty("thread.mdc.enabled", "true"));
 
     @Override
     protected String getMatcherClassName() {
